4875eee46a0710db301e3bf849fd162945e652db
Brian Wynn <b_wynn@apple.com>
2016-03-08 12:02:01 -0800

    Getting some of the initial controller and scope content defined.

--- public/js/cart_app.js
+++ public/js/cart_app.js
@@ -1,4 +1,10 @@
-angular.module('myApp', [])
-  .controller('shoppingController', function($scope) {
-
-  });
+angular.module('myApp', ['ngRoute'])
+  .controller('shoppingController', ['$scope', '$http', '$window', function($scope, $http, $window) {
+    $scope.months = [1,2,3,4,5,6,7,8,9,10,11,12];
+    $scope.years = [2016, 2017, 2018, 2019, 2020, 2021, 2022];
+    $scope.content = '/static/products.html';
+    $http.get('/products/get').then(function(data) {
+      $scope.products = data;
+      $scope.product = data[0];
+    });
+  }]);
